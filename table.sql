f9d84b88a6a6f2717ea505779d37d1433e993f6a
JamesMwangi <mwangijames2014@outlook.com>
2015-12-22 15:11:27 +0300

    First commit

---
+++ table.sql
@@ -0,0 +1,24 @@
+﻿--
+-- Table structure for table `internet_shop`
+--
+
+CREATE TABLE IF NOT EXISTS `internet_shop` (
+  `id` int(6) NOT NULL auto_increment,
+  `img` varchar(32) collate utf8_unicode_ci NOT NULL default '',
+  `name` varchar(64) collate utf8_unicode_ci NOT NULL default '',
+  `description` text collate utf8_unicode_ci NOT NULL,
+  `price` double NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `img` (`img`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `internet_shop`
+--
+
+INSERT INTO `internet_shop` VALUES(1, 'test.jpg', 'Kenyan black', 'Kenyan black', 200);
+INSERT INTO `internet_shop` VALUES(2, 'test1.jpg', 'Xili Red', 'Xilli Red.', 1200);
+INSERT INTO `internet_shop` VALUES(3, 'test2.jpg', 'Prada Gold', 'Prada Gold.', 400);
+INSERT INTO `internet_shop` VALUES(4, 'test3.jpg', 'Tianshan Red', 'Tianshan Red.', 49);
+INSERT INTO `internet_shop` VALUES(5, 'test4.jpg', 'wavw flower', 'Wave flower.', 99);
+INSERT INTO `internet_shop` VALUES(6, 'test5.jpg', 'kitchen sink', 'Kitchen sink', 300);
